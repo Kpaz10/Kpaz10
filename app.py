ba54da6698646c6e20ce9595cfea1dc726db9fc6
Kathy Paz <kathypaz1993@gmail.com>
2022-06-23 20:14:01 -0500

    Proyecto inicial.

---
+++ app.py
@@ -0,0 +1,15 @@
+
+
+class Calculadora:
+    def sumar(self,num1,num2):
+        return num1+num2
+
+
+
+
+
+instance=Calculadora()
+
+resultado=instance.sumar(10,19)
+
+print ("la suma es:", resultado)
